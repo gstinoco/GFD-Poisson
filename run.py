7da5fd134f37d26bcda4976b54ec0742bd08e3f7
gstinoco <gstinoco@gmail.com>
2023-01-17 10:38:55 -0600

    Results

--- run.py
+++ run.py
@@ -23,8 +23,8 @@ import Poisson_2D
 
 # Region data is loaded.
 # Triangulation or unstructured cloud of points to work in.
-region = 'CAB'
-cloud = '3'
+region = 'CHA'
+cloud = '4'
 # Mesh size to work in.
 mesh = '81'
 # This region can be changed for any other triangulation, unstructured cloud of points or mesh on Regions/ or with any other region with the same file data structure.
