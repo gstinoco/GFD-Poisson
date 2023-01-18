ea9a778ac19bf003a50d26768a91612709f07717
gstinoco <gstinoco@gmail.com>
2023-01-17 19:28:46 -0600

    Neighbors routine improved.

--- Neighbors.py
+++ Neighbors.py
@@ -17,7 +17,6 @@
 # Routines to find neighboring nodes in a triangulation or in a cloud of points.
 
 import numpy as np
-import math
 
 def Triangulation(p, tt, nvec):
     # Triangulation
@@ -33,15 +32,16 @@ def Triangulation(p, tt, nvec):
 
     # Variable initialization
     m   = len(p[:,0])                                                               # The size if the triangulation is obtained.
-    vec = np.zeros([m, nvec])-1                                                     # The array for the neighbors is initialized.
+    vec = np.zeros([m, nvec], dtype=int)-1                                          # The array for the neighbors is initialized.
 
     # Neighbor search
     for i in np.arange(m):                                                          # For each of the nodes.
         kn   = np.argwhere(tt == i)                                                 # Search in which triangles the node appears.
         vec2 = np.setdiff1d(tt[kn[:,0]], i)                                         # Neighbors are stored inside vec2.
         vec2 = np.vstack([vec2])                                                    # Convert vec2 to a column.
-        nvec = sum(vec2[0,:] != -1)                                                 # The number of neighbors of the node is calculated.
-        for j in np.arange(nvec):                                                   # For each of the nodes.
+        nvec2 = sum(vec2[0,:] != -1)                                                # The number of neighbors of the node is calculated.
+        nnvec = np.minimum(nvec, nvec2)
+        for j in np.arange(nnvec):                                                   # For each of the nodes.
             vec[i,j] = vec2[0,j]                                                    # Neighbors are saved.   
     return vec
 
@@ -59,7 +59,7 @@ def Cloud(p, pb, nvec):
 
     # Variable initialization
     m    = len(p[:,0])                                                              # The size if the triangulation is obtained.
-    vec  = np.zeros([m, nvec],dtype=int)-1                                           # The array for the neighbors is initialized.
+    vec  = np.zeros([m, nvec], dtype=int)-1                                         # The array for the neighbors is initialized.
     dist = 0                                                                        # Maximum distance between the boundary nodes.
 
     # Search for the maximum distance between the boundary nodes
@@ -67,9 +67,9 @@ def Cloud(p, pb, nvec):
     yb = pb[:,1]                                                                    # y coordinates of the boundary nodes.
     m2 = len(xb)                                                                    # Total number of boundary nodes.
     for i in np.arange(m2-1):
-        d    = math.sqrt((xb[i] - xb[i+1])**2 + (yb[i] - yb[i+1])**2)               # The distance between a boundary node and the next one.
+        d    = np.sqrt((xb[i] - xb[i+1])**2 + (yb[i] - yb[i+1])**2)                 # The distance between a boundary node and the next one.
         dist = max(dist,d)                                                          # Maximum distance search.
-    d    = math.sqrt((xb[m2-1] - xb[0])**2 + (yb[m2-1] - yb[0])**2)                 # The distance between the last and the first boundary nodes.
+    d    = np.sqrt((xb[m2-1] - xb[0])**2 + (yb[m2-1] - yb[0])**2)                   # The distance between the last and the first boundary nodes.
     dist = (8/7)*max(dist,d)                                                        # Maximum distance search.
 
     # Search of the neighbor nodes
@@ -81,7 +81,7 @@ def Cloud(p, pb, nvec):
             if i != j:                                                              # Check that we are not working with the central node.
                 x1 = p[j,0]                                                         # x coordinate of the possible neighbor.
                 y1 = p[j,1]                                                         # y coordinate of the possible neighbor.
-                d  = math.sqrt((x - x1)**2 + (y - y1)**2)                           # Distance from the possible neighbor to the central node.
+                d  = np.sqrt((x - x1)**2 + (y - y1)**2)                             # Distance from the possible neighbor to the central node.
                 if d < dist:                                                        # If the distance is smaller or equal to the tolerance distance.
                     if temp < nvec:                                                 # If the number of neighbors is smaller than nvec.
                         vec[i,temp] = j                                             # Save the neighbor.
@@ -119,9 +119,9 @@ def Cloud_Adv(p, pb, nvec, a, b):
     yb = pb[:,1]                                                                    # y coordinates of the boundary nodes.
     m2 = len(xb)                                                                    # Total number of boundary nodes.
     for i in np.arange(m2-1):
-        d    = math.sqrt((xb[i] - xb[i+1])**2 + (yb[i] - yb[i+1])**2)               # The distance between a boundary node and the next one.
+        d    = np.sqrt((xb[i] - xb[i+1])**2 + (yb[i] - yb[i+1])**2)                 # The distance between a boundary node and the next one.
         dist = max(dist,d)                                                          # Maximum distance search.
-    d    = math.sqrt((xb[m2-1] - xb[0])**2 + (yb[m2-1] - yb[0])**2)                 # The distance between the last and the first boundary nodes.
+    d    = np.sqrt((xb[m2-1] - xb[0])**2 + (yb[m2-1] - yb[0])**2)                   # The distance between the last and the first boundary nodes.
     dist = (8/7)*max(dist,d)                                                        # Maximum distance search.
 
     # Search of the neighbor nodes
@@ -136,7 +136,7 @@ def Cloud_Adv(p, pb, nvec, a, b):
                 xt = x1 - x                                                         # The "new" origin on x.
                 yt = y1 - y                                                         # The "new" origin on y.
                 di = np.sign(np.dot([xt, yt],[a, b]))                               # Check the direction between (xt, yt) and (a,b).
-                d  = math.sqrt((x - x1)**2 + (y - y1)**2)                           # Distance from the possible neighbor to the central node.
+                d  = np.sqrt((x - x1)**2 + (y - y1)**2)                             # Distance from the possible neighbor to the central node.
                 if di == -1 and d < dist:                                           # If the direction is opposite and the distance is smaller or equal to the tolerance distance.
                     if temp <= nvec:                                                # If the number of neighbors is smaller than nvec.
                         vec[i,temp] = j                                             # Save the neighbor.
@@ -144,7 +144,7 @@ def Cloud_Adv(p, pb, nvec, a, b):
                     else:                                                           # If the number of neighbors is greater than nvec.
                         x2 = p[vec[i,:],0]                                          # x coordinates of the current neighbor nodes.
                         y2 = p[vec[i,:],1]                                          # y coordinates of the current neighbor nodes.
-                        d2 = math.sqrt((x - x2)**2 + (y - y2)**2)                   # The total distance from all the neighbors to the central node.
+                        d2 = np.sqrt((x - x2)**2 + (y - y2)**2)                     # The total distance from all the neighbors to the central node.
                         I  = np.argmax(d2)                                          # Look for the greatest distance.
                         if d < d2[I]:                                               # If the new node is closer than the farthest neighbor.
                             vec[i,I] = j                                            # The new neighbor replace the farthest one.
