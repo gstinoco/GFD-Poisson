f1078e7d83a24e9ca68be839a8151dd0431491a3
gstinoco <gstinoco@gmail.com>
2023-01-19 08:17:29 -0600

    Results

--- run_all.py
+++ run_all.py
@@ -64,16 +64,16 @@ def f(x,y):
     return fun
 
 # Poisson 2D computed in a logically rectangular mesh
-phi_ap, phi_ex = Poisson_2D.Mesh(x, y, phi, f)
-er = Errors.Mesh_Static(x, y, phi_ap, phi_ex)
-print('The mean square error in the mesh', region, 'with', mesh, 'points per side is: ', er)
-Graph.Mesh_Static_sav(x, y, phi_ap, phi_ex, nomm)
+#phi_ap, phi_ex = Poisson_2D.Mesh(x, y, phi, f)
+#er = Errors.Mesh_Static(x, y, phi_ap, phi_ex)
+#print('The mean square error in the mesh', region, 'with', mesh, 'points per side is: ', er)
+#Graph.Mesh_Static_sav(x, y, phi_ap, phi_ex, nomm)
 
 # Poisson 2D computed in a triangulation
-phi_ap, phi_ex, vec = Poisson_2D.Triangulation(p, pb, tt, phi, f)
-er = Errors.Cloud_Static(p, vec, phi_ap, phi_ex)
-print('The mean square error in the triangulation', region, 'with size', cloud, 'is: ', er)
-Graph.Cloud_Static_sav(p, tt, phi_ap, phi_ex, nomt)
+#phi_ap, phi_ex, vec = Poisson_2D.Triangulation(p, pb, tt, phi, f)
+#er = Errors.Cloud_Static(p, vec, phi_ap, phi_ex)
+#print('The mean square error in the triangulation', region, 'with size', cloud, 'is: ', er)
+#Graph.Cloud_Static_sav(p, tt, phi_ap, phi_ex, nomt)
 
 # Poisson 2D computed in an unstructured cloud of points
 phi_ap, phi_ex, vec = Poisson_2D.Cloud(p, pb, phi, f)
