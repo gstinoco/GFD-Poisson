b3f2bfed1ae45f088978644b7d7b934d4a3084f1
gstinoco <gstinoco@gmail.com>
2023-01-27 13:01:14 -0600

    Changed for new Cloud structure.

--- run_triangulations.py
+++ run_triangulations.py
@@ -14,7 +14,7 @@
 # Last Modification:
 #   January, 2023.
 
-import math
+import numpy as np
 from scipy.io import loadmat
 import Scripts.Errors as Errors
 import Scripts.Graph as Graph
@@ -49,11 +49,11 @@ for reg in regions:
         #   f = 10e^{2x+y}
 
         def phi(x,y):
-            fun = 2*math.exp(2*x+y)
+            fun = 2*np.exp(2*x+y)
             return fun
 
         def f(x,y):
-            fun = 10*math.exp(2*x+y)
+            fun = 10*np.exp(2*x+y)
             return fun
 
         # Poisson 2D computed in a triangulation
